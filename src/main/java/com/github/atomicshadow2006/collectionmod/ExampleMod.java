5ff3218f2a62ef62f716e23f1298173d303663dc
GitHub Action <action@github.com>
2024-07-25 01:10:37 +0000

    Initialize template repository proper
    Set package name to com.github.atomicshadow2006.collectionmod
    Set project name to collectionmod
    Set modid to collectionmod

--- src/main/java/com/github/atomicshadow2006/collectionmod/ExampleMod.java
+++ src/main/java/com/github/atomicshadow2006/collectionmod/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.atomicshadow2006.collectionmod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "collectionmod", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
